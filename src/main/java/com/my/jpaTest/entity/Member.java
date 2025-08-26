4b9cabf57ce0f924df39661d56a41acc15564311
noeyos-p <krapnoeyos@gmail.com>
2025-08-26 12:03:36 +0900

    양방향 연관관계 설정

--- src/main/java/com/my/jpaTest/entity/Member.java
+++ src/main/java/com/my/jpaTest/entity/Member.java
@@ -1,7 +1,6 @@
 package com.my.jpaTest.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -13,4 +12,9 @@ public class Member {
     @Id
     private String memberId;
     private String name;
+
+    // 팀 정보를 갖는 연관관계를 구성
+    @ManyToOne
+    @JoinColumn(name = "teamId")
+    private Team team;
 }
