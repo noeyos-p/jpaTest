73642c8bf2a03e539d2c081f7c21836448d8130a
noeyos-p <krapnoeyos@gmail.com>
2025-08-26 12:03:36 +0900

    양방향 연관관계 설정

---
+++ src/test/java/com/my/jpaTest/service/RelationTestServiceTest.java
@@ -0,0 +1,70 @@
+package com.my.jpaTest.service;
+
+import com.my.jpaTest.entity.Member;
+import com.my.jpaTest.entity.Team;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class RelationTestServiceTest {
+    @Autowired
+    RelationTestService relationTestService;
+
+    @Test
+    @DisplayName("MemberAndTeam Insert Test")
+    void insertTest() {
+        relationTestService.insertMemberAndTeam();
+    }
+
+    @Test
+    @DisplayName("단방향 연관관계 설정 후 팀 이름 찾기")
+    void findTeamName() {
+        // jang의 정보를 가져온다.
+        Member jang = em.find(Member.class, "jang");
+        // 팀 정보 출력
+        System.out.println("팀 이름 : " + jang.getTeam().getTeamName());
+    }
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("단방향 연관관계 설정 후 팀 이름 찾기")
+    void saveBothData() {
+        relationTestService.insertBothRelation();
+    }
+
+    @Test
+    @DisplayName("씨름팀 멤버 출력하기")
+    void 씨름팀멤버출력() {
+        Team team = em.find(Team.class, "ssirum");
+        List<Member> members = team.getMemberList();
+        for (Member m : members) {
+            System.out.println(m.getName());
+        }
+    }
+
+    // 장원영(key : jang)의 팀 이름을 알고 싶어요
+    /*@Test
+    @DisplayName("ID:jang의 팀 이름 찾기 테스트")
+    void findTeamName() {
+        // 연관관계가 없으면 한 없이 찾아들어가서 원하는 자료를 찾아야함
+        // 1. member에서 jang을 찾아온다.
+        Member jang = em.find(Member.class, "jang");
+        // 2. member의 teamId로 team에서 검색한다
+        Team team = em.find(Team.class, jang.getTeamId());
+        System.out.println("Team Name : " + team.getTeamName());
+    }*/
+
+    // Unique index or primary key violation: "PUBLIC.PRIMARY_KEY_2 ON PUBLIC.TEAM(TEAM_ID) VALUES ( /* 1 */ 'ive' )"; SQL statement:
+    // 이미 데이터 베이스에 존재하는 데이터를 또 집어넣으려고 했다 라는 오류
+}
